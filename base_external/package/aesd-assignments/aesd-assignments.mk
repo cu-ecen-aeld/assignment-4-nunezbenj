b0e81d00b623887c361cdb7be37edad1ac434451
nunezbenj <nunezbenj@ubuntu20.04.myguest.virtualbox.org>
2025-07-27 16:57:54 -0600

    final version assignment 4

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,11 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 36e5bc21200fff9a98c8bf1bb9d8f29e553a2280
+
+# Usihg a tag instead of a commit as the VERSION
+#AESD_ASSIGNMENTS_VERSION = 36e5bc21200fff9a98c8bf1bb9d8f29e553a2280
+AESD_ASSIGNMENTS_VERSION = assignment-4-complete
+
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
